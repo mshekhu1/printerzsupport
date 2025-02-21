dc13f5018b0a8f47b6d08cb38a81b3fdba55578c
Mohammad Shekhu <geniusshekhu@gmail.com>
2025-02-22 01:39:35 +0530

    Update changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>Contact us: <a href="support@printerzsupport.com">support@printerzsupport.com</a> | Phone: (786) 753-8470</p>
+        <p>Contact us: <a href="support@printerzsupport.com">support@printerzsupport.com</a> | Phone: +1-786-753-8470</p>
       </div>
     </footer>
   );
