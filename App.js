f375e139ac869e92ef8d2a8feed33d60d309b957
Mohammad Shekhu <geniusshekhu@gmail.com>
2025-02-22 00:39:15 +0530

    Updated phone number format to +1-786-753-8470

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './components/Footer';
+// ...existing code...
+
+function App() {
+    return (
+        <div className="App">
+            {/* ...existing code... */}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Contact us: <a href="support@printerzsupport.com">support@printerzsupport.com</a> | Phone: (786) 753-8470</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
